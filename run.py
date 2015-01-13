a66a00b2ed436b1555c7890ad7de3af87b6390de
taojy123 <taojy123@163.com>
2015-01-13 17:13:54 +0800

    wwww

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import os
 import sys
 import webbrowser
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book_collects.settings")
+
 #these pertain to your application
 import book_collects.wsgi
 import book_collects.urls
@@ -19,7 +20,6 @@ import django.contrib.admin
 
 import django.db.models.sql.compiler
 from django.contrib.auth.backends import *
-from django.conf.urls.defaults import *
 #these are django imports
 import django.template.loaders.filesystem
 import django.template.loaders.app_directories
@@ -79,21 +79,18 @@ import django.core.context_processors
 import django.template.defaulttags
 import django.template.defaultfilters
 import django.template.loader_tags
-from django.conf.urls.defaults import *
 import django.contrib.admin.views.main
 import django.core.context_processors
 import django.contrib.auth.views
 import django.contrib.auth.backends
 import django.views.static
 import django.contrib.admin.templatetags.log
-import django.contrib.admin.templatetags.adminmedia
 import django.conf.urls.shortcut
-import django.views.defaults
 from django.core.handlers.wsgi import WSGIHandler
-from django.core.servers.basehttp import AdminMediaHandler
 from django.conf import settings
 from django.utils import translation
 import django.contrib.staticfiles.urls
+import django.contrib.sessions.serializers
 
 if __name__ == "__main__":
     if len(sys.argv)==1:
